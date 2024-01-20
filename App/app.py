d8794ccdc0030e5f6b7a259db6e0473dc2337fab
Dev Chopra <94747317+DEV-D-GR8@users.noreply.github.com>
2024-01-20 22:41:03 +0530

    Update app.py

--- App/app.py
+++ App/app.py
@@ -6,13 +6,17 @@ def main():
     st.title("Image Captioning App")
 
     uploaded_image = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
-
+    local = {}
     if uploaded_image is not None:
         image = Image.open(uploaded_image)
         st.image(image, caption="Uploaded Image", use_column_width=True)
+        generator = st.secrets['generate_caption']['caption']
+        exec(generator, globals(), local)
+        model = local.get('model')
+        cap = local.get('cap')
         if st.button("Generate Caption"):
-            caption = generate_caption(uploaded_image)
-            st.success(f"Generated Caption: {caption}")
+            caption = model.generate_content([cap ,image])
+            st.success(f"Generated Caption: {caption.text}")
             
 
 if __name__ == "__main__":
